3d494c8c30998776b28fdbaa4def271089510144
Irfan Asif <hashtag.irfan@gmail.com>
2021-09-09 00:18:45 +0530

    added chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import ExpensesList from "./ExpensesList";
-import Card from "../UI/Card";
-import ExpensesFilter from "./ExpensesFilter";
-import "./Expenses.css";
-
-
+import ExpensesList from './ExpensesList';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021');
@@ -20,8 +19,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  
-
   return (
     <div>
       <Card className='expenses'>
@@ -29,11 +26,11 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpensesList filteredExpenses={filteredExpenses}/>
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList filteredExpenses={filteredExpenses} />
       </Card>
     </div>
   );
 };
 
 export default Expenses;
-
